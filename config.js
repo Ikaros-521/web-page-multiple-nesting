82255c595444fc67adcacd53dd8c4c16b9940e49
Ikaros <327209194@qq.com>
2023-04-20 19:57:15 +0800

    补充链接

--- config.js
+++ config.js
@@ -2,5 +2,7 @@
 let config_data = [
     "https://cc.ai55.cc/",
     "https://chat.aidutu.cn/#/chat/1002",
-    "https://chat.wuguokai.cn/#/chat/1681989770900"
+    "https://chat.wuguokai.cn/#/chat/1681989770900",
+    "https://chat.gptplus.one/#/chat/1002",
+    "https://chat.wobcw.com/"
 ];
